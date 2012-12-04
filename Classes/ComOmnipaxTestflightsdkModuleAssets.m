062bdc3cbb184db8099db849413ce529b2e6d914
Aidan Goettsch <yayestechlab@me.com>
2012-12-03 19:44:33 -0500

    first commit

---
+++ Classes/ComOmnipaxTestflightsdkModuleAssets.m
@@ -0,0 +1,15 @@
+/**
+ * This is a generated file. Do not edit or your changes will be lost
+ */
+#import "ComOmnipaxTestflightsdkModuleAssets.h"
+
+extern NSData* filterDataInRange(NSData* thedata, NSRange range);
+
+@implementation ComOmnipaxTestflightsdkModuleAssets
+
+- (NSData*) moduleAsset
+{
+	return nil;
+}
+
+@end
